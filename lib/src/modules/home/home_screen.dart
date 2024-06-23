3f8e54c2e2e6a42c42902c340c35a48550caced9
erfan soltanzadeh <soltanzadeh836@gmail.com>
2024-06-23 14:10:04 +0330

    home_screen.dart created in modules/home

---
+++ lib/src/modules/home/home_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../logic/url_provider.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _launchUrlOnStart();
+  }
+
+  void _launchUrlOnStart() async {
+    final urlProvider = Provider.of<UrlProvider>(context, listen: false);
+    try {
+      await urlProvider.launchWebsite();
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(e.toString())),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            final urlProvider =
+                Provider.of<UrlProvider>(context, listen: false);
+            try {
+              await urlProvider.launchWebsite();
+            } catch (e) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text(e.toString())),
+              );
+            }
+          },
+          child: const Icon(Icons.home_rounded),
+        ),
+      ),
+    );
+  }
+}
