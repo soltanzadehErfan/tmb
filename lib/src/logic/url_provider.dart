922260a3e7dfd636f3692f6158bf4ffb018fe2a9
erfan soltanzadeh <soltanzadeh836@gmail.com>
2024-06-24 13:11:19 +0330

    docs added to url_provider.dart

--- lib/src/logic/url_provider.dart
+++ lib/src/logic/url_provider.dart
@@ -2,13 +2,23 @@ import 'package:tmb/src/utils/safe_provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 /// [UrlProvider]:
-/// provides a method for launching the  https://tambord.com/
+/// provides a method for launching the urls on default web browser of users device
 class UrlProvider extends SafeProvider {
-  final Uri _url = Uri.parse('https://tambord.com/');
+  final Uri _tambordUrl = Uri.parse('https://tambord.com/');
 
   Future<void> launchTambord() async {
-    if (!await launchUrl(_url, mode: LaunchMode.inAppWebView)) {
-      throw Exception('Could not launch $_url');
+    if (!await launchUrl(_tambordUrl, mode: LaunchMode.inAppWebView)) {
+      throw Exception('Could not launch $_tambordUrl');
     }
   }
 }
+
+// import 'package:url_launcher/url_launcher.dart';
+//
+// final Uri _url = Uri.parse('https://tambord.com/');
+//
+// Future<void> launchTambord() async {
+//   if (!await launchUrl(_url, mode: LaunchMode.inAppWebView)) {
+//     throw Exception('Could not launch $_url');
+//   }
+// }
