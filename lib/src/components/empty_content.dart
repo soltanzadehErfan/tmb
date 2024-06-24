620ff751facb66260677851ebe3bce047708df2e
erfan soltanzadeh <soltanzadeh836@gmail.com>
2024-06-24 15:49:23 +0330

    empty_content.dart customized

--- lib/src/components/empty_content.dart
+++ lib/src/components/empty_content.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
 
+/// [EmptyContent]: this component gives a empty content widget for you
+/// optional String message: you can add your optional message
 class EmptyContent extends StatelessWidget {
-  const EmptyContent({super.key});
+  final String? message;
+
+  const EmptyContent({super.key, this.message});
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox();
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.network_check_rounded,
+              size: 80,
+              color: Colors.grey[400],
+            ),
+            const SizedBox(height: 16),
+            Text(
+              message ?? 'no data',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey[600],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
