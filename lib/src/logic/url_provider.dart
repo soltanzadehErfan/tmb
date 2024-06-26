8d42756aca82369f8b291d110f7ef2b70b344598
erfan soltanzadeh <soltanzadeh836@gmail.com>
2024-06-26 09:18:05 +0330

    provider to method in url_provider.dart

--- lib/src/logic/url_provider.dart
+++ lib/src/logic/url_provider.dart
@@ -1,24 +1,24 @@
 import 'package:tmb/src/utils/safe_provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-/// [UrlProvider]:
-/// provides a method for launching the urls on default web browser of users device
-class UrlProvider extends SafeProvider {
-  final Uri _tambordUrl = Uri.parse('https://tambord.com/');
-
-  Future<void> launchTambord() async {
-    if (!await launchUrl(_tambordUrl, mode: LaunchMode.inAppWebView)) {
-      throw Exception('Could not launch $_tambordUrl');
-    }
-  }
-}
-
-// import 'package:url_launcher/url_launcher.dart';
+// /// [UrlProvider]:
+// /// provides a method for launching the urls on default web browser of users device
+// class UrlProvider extends SafeProvider {
+//   final Uri _tambordUrl = Uri.parse('https://tambord.com/');
 //
-// final Uri _url = Uri.parse('https://tambord.com/');
-//
-// Future<void> launchTambord() async {
-//   if (!await launchUrl(_url, mode: LaunchMode.inAppWebView)) {
-//     throw Exception('Could not launch $_url');
+//   Future<void> launchTambord() async {
+//     if (!await launchUrl(_tambordUrl, mode: LaunchMode.inAppWebView)) {
+//       throw Exception('Could not launch $_tambordUrl');
+//     }
 //   }
 // }
+
+final Uri _url = Uri.parse('https://tambord.com/');
+
+/// [launchTambord]:
+/// This method is for launching the Tambord url on default web browser of users device inAppWebView
+Future<void> launchTambord() async {
+  if (!await launchUrl(_url, mode: LaunchMode.inAppWebView)) {
+    throw Exception('Could not launch $_url');
+  }
+}
