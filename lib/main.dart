fa5b43c4328a8e93073e60023f6160d3c86474b3
erfan soltanzadeh <soltanzadeh836@gmail.com>
2024-06-26 09:19:21 +0330

    multiProvider removed from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,15 @@ import 'package:provider/provider.dart';
 import 'src/core/my_app.dart';
 import 'src/logic/url_provider.dart';
 
-void main() => runApp(
-      MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (_) => UrlProvider()),
-        ],
-        child: const MyApp(),
-      ),
+// Future<void> main() async => runApp(
+//       MultiProvider(
+//         providers: [
+//           ChangeNotifierProvider(create: (_) => UrlProvider()),
+//         ],
+//         child: const MyApp(),
+//       ),
+//     );
+
+Future<void> main() async => runApp(
+      const MyApp(),
     );
