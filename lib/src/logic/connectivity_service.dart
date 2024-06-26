aa9e18ce9de2300855beeeb6326e0b26b5a81972
erfan soltanzadeh <soltanzadeh836@gmail.com>
2024-06-26 09:14:17 +0330

    vpn checker added to connectivity_service.dart

--- lib/src/logic/connectivity_service.dart
+++ lib/src/logic/connectivity_service.dart
@@ -1,13 +1,16 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 
-/// [connectivityChecker]: checks for user's device network connectivity status
+/// [connectivityChecker]: Checks for user's device network connectivity status and VPN usage
 Future<String> connectivityChecker() async {
-  final List<ConnectivityResult> connectivityResult = await Connectivity().checkConnectivity();
+  final List<ConnectivityResult> connectivityResult =
+      await Connectivity().checkConnectivity();
 
   if (connectivityResult.contains(ConnectivityResult.mobile) ||
       connectivityResult.contains(ConnectivityResult.wifi) ||
       connectivityResult.contains(ConnectivityResult.ethernet)) {
     return 'online';
+  } else if (connectivityResult.contains(ConnectivityResult.vpn)) {
+    return 'vpn';
   } else {
     return 'offline';
   }
