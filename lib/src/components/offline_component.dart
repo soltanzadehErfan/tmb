e72c0614a9f1396954c857eac957b0881e409d74
erfan soltanzadeh <soltanzadeh836@gmail.com>
2024-06-26 11:57:16 +0330

    offline_component.dart

--- lib/src/components/offline_component.dart
+++ lib/src/components/offline_component.dart
@@ -12,40 +12,33 @@ class OfflineComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgPicture.asset(
-                  tambordLogo,
-                ),
-                const SizedBox(height: 64),
-                const SpinKitWaveSpinner(
-                  color: Colors.white,
-                  trackColor: Colors.blue,
-                  size: 100.0,
-                  child: Icon(Icons.network_check_rounded),
-                ),
-                const SizedBox(height: 8),
-                const TextButton(
-                  onPressed: null,
-                  child: Text(
-                    'You are offline :(',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.grey,
-                    ),
-                  ),
-                ),
-              ],
+    return const Center(
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 64),
+            SpinKitWaveSpinner(
+              color: Colors.white,
+              trackColor: Colors.blue,
+              size: 100.0,
+              child: Icon(Icons.network_check_rounded),
             ),
-          ),
-        )
-      ],
+            SizedBox(height: 8),
+            TextButton(
+              onPressed: null,
+              child: Text(
+                'You are offline :(',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
