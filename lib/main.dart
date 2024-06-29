7a7c9e3f1e27b507595afbc4beb834de5f42c5bf
erfan soltanzadeh <soltanzadeh836@gmail.com>
2024-06-29 14:01:08 +0330

    main.dart minor cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,7 @@ import 'package:flutter/material.dart';
 import 'src/core/my_app.dart';
 import 'src/logic/notification_service.dart';
 
-// Future<void> main() async => runApp(
-//       MultiProvider(
-//         providers: [
-//           ChangeNotifierProvider(create: (_) => UrlProvider()),
-//         ],
-//         child: const MyApp(),
-//       ),
-//     );
-
-// Future<void> main() async => runApp(
-
-//       const MyApp(),
-//     );
-
+/// [main] Responsible for initializing and execution of the application
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await NotificationService().init();
