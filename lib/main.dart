fc287ffad1872607d0d167b1e7370b420964c745
erfan soltanzadeh <soltanzadeh836@gmail.com>
2024-06-29 14:07:17 +0330

    main.dart docs added

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 import 'src/core/my_app.dart';
 import 'src/logic/notification_service.dart';
 
-/// [main] Responsible for initializing and execution of the application
+/// [main] The main function is responsible for initializing and executing the application.
+/// It ensures Flutter bindings are initialized, initializes the notification service,
+/// and then runs the main application widget [MyApp].
 Future<void> main() async {
+  // Ensures Flutter framework is properly initialized before executing any other code.
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize the notification service to set up local notifications.
   await NotificationService().init();
+
+  // Runs the main application widget [MyApp].
   runApp(const MyApp());
 }
