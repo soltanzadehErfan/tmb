0fc279fd6443fffb16011e9aa714b966032fb211
erfan soltanzadeh <soltanzadeh836@gmail.com>
2024-06-29 14:03:01 +0330

    safe_provider.dart docs added

--- lib/src/utils/safe_provider.dart
+++ lib/src/utils/safe_provider.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 
+/// A [SafeProvider] class that extends [ChangeNotifier] to safely notify listeners and handle disposal.
+///
+/// This class ensures that listeners are only notified if the provider has not been disposed,
+/// preventing potential issues that arise from trying to notify listeners after disposal.
 class SafeProvider extends ChangeNotifier {
   bool _isDisposed = false;
 
+  /// Overrides the [notifyListeners] method to ensure it only notifies listeners
+  /// if the provider has not been disposed.
   @override
   void notifyListeners() {
     if (_isDisposed) return;
     super.notifyListeners();
   }
 
+  /// Overrides the [dispose] method to mark the provider as disposed and
+  /// then calls the superclass's dispose method.
   @override
   void dispose() {
     _isDisposed = true;
