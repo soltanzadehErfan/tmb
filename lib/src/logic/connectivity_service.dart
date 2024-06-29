f29dd55784e33aefbd3090c0d2d42b7fd1a989a0
erfan soltanzadeh <soltanzadeh836@gmail.com>
2024-06-29 14:17:26 +0330

    connectivity_service.dart docs added

--- lib/src/logic/connectivity_service.dart
+++ lib/src/logic/connectivity_service.dart
@@ -1,16 +1,18 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 
-/// [connectivityChecker]: Checks for user's device network connectivity status and VPN usage
+/// [connectivityChecker]: Checks for user's device network connectivity status
+/// Returns 'online' if the device is connected to a mobile, Wi-Fi, or ethernet network
+/// Returns 'offline' otherwise
 Future<String> connectivityChecker() async {
+  // Check the connectivity status of the device
   final List<ConnectivityResult> connectivityResult =
       await Connectivity().checkConnectivity();
 
+  // Determine the connection status based on the connectivity result
   if (connectivityResult.contains(ConnectivityResult.mobile) ||
       connectivityResult.contains(ConnectivityResult.wifi) ||
       connectivityResult.contains(ConnectivityResult.ethernet)) {
     return 'online';
-  } else if (connectivityResult.contains(ConnectivityResult.vpn)) {
-    return 'vpn';
   } else {
     return 'offline';
   }
