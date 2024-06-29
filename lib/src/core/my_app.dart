72d45b629227bba0213260cc270b776e2dfda34d
erfan soltanzadeh <soltanzadeh836@gmail.com>
2024-06-29 14:14:57 +0330

    my_app.dart docs added

--- lib/src/core/my_app.dart
+++ lib/src/core/my_app.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-
 import '../modules/home/home_screen.dart';
 
+/// [MyApp] This is the main application widget.
+/// It sets up the application with a dark theme and initializes the home screen.
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
