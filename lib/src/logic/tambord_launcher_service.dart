98a755f170a32aa73deb36912a66df34f102275d
erfan soltanzadeh <soltanzadeh836@gmail.com>
2024-07-01 10:57:16 +0330

    minor changes in tambord_launcher_service.dart

--- lib/src/logic/tambord_launcher_service.dart
+++ lib/src/logic/tambord_launcher_service.dart
@@ -1,17 +1,5 @@
 import 'package:url_launcher/url_launcher.dart';
 
-// /// [UrlProvider]:
-// /// provides a method for launching the urls on default web browser of users device
-// class UrlProvider extends SafeProvider {
-//   final Uri _tambordUrl = Uri.parse('https://tambord.com/');
-//
-//   Future<void> launchTambord() async {
-//     if (!await launchUrl(_tambordUrl, mode: LaunchMode.inAppWebView)) {
-//       throw Exception('Could not launch $_tambordUrl');
-//     }
-//   }
-// }
-
 final Uri _url = Uri.parse('https://tambord.com/');
 
 /// [launchTambord]:
