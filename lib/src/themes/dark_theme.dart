faa2630524605f6a1a1574c383c1218b5cde006f
erfan soltanzadeh <soltanzadeh836@gmail.com>
2024-07-01 10:53:29 +0330

    light_theme.dart dark_theme.dart created

---
+++ lib/src/themes/dark_theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppThemeDataDark {
+  static final ThemeData darkTheme = ThemeData(fontFamily: 'IranSans');
+}
---
+++ lib/src/themes/light_theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppThemeDataLight {
+  static final ThemeData lightTheme = ThemeData(fontFamily: 'IranSans');
+}
